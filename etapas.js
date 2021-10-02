75749a9a154be403a8e3c30b9c142dbd77d0d718
lucianamcedro <luciana_cedro@hotmail.com>
2021-10-02 02:26:56 -0300

    firts commit

---
+++ etapas.js
@@ -0,0 +1,66 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '00000',
+                nome: 'Bart',
+                partido: 'Malandragem',
+                fotos: [
+                    {url: 'bart.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '01234',
+                nome: 'Homer',
+                partido: 'DUFF',
+                fotos: [
+                    {url: 'homer.jpg', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '01010',
+                nome: 'Steven',
+                partido: 'Universe',
+                fotos: [
+                    {url: 'steven.png', legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero: '23232',
+                nome: 'Polar',
+                partido: 'Sem Curso',
+                fotos: [
+                    {url: 'polar.png', legenda: 'Vereador'}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '25',
+                nome: 'Rick',
+                partido: 'Dominadores Do Mundo',
+                vice: 'Morty',
+                fotos: [
+                    {url: 'rick.jpg', legenda: 'Prefeito'},
+                    {url: 'morty.png', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: '33',
+                nome: 'Liza',
+                partido: 'Os Simpsons',
+                vice: 'Marge',
+                fotos: [
+                    {url: 'liza.png', legenda: 'Prefeito'},
+                    {url: 'marge1.png', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            }
+        ]
+    }
+]
